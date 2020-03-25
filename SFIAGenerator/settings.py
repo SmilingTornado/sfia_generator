306650aa6ea40ede8a11cf4b7e30859554c00266
Ubuntu <ubuntu@ip-172-31-44-178.eu-west-2.compute.internal>
2020-03-25 13:33:47 +0000

    static fixes

--- SFIAGenerator/settings.py
+++ SFIAGenerator/settings.py
@@ -15,9 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
-)
+    os.path.join(BASE_DIR, 'static'),
+    )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -28,7 +30,7 @@ SECRET_KEY = '!ea9jb%#2c#*+gg0rsok+*llvyv)lwqx1q=ebc-j)ki4rw*)ey'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.130.244.39']
 
 
 # Application definition
@@ -116,9 +118,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
