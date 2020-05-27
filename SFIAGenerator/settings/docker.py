1062f523d64e844be7377c221f505406b1286c2b
Worawat <KimparaW@cardiff.ac.uk>
2020-05-27 18:27:40 +0700

    modifed settings file and cleaned up some names

--- SFIAGenerator/settings/docker.py
+++ SFIAGenerator/settings/docker.py
@@ -2,7 +2,7 @@ from SFIAGenerator.settings.base import *
 ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
