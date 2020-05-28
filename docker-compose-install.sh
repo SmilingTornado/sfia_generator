0fc8178021186afbaa778f154d8a19e9428fa5cc
Worawat <KimparaW@cardiff.ac.uk>
2020-05-28 14:32:26 +0700

    clean up

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get remove docker docker-engine docker.io
+apt-get remove docker docker-engine docker.io docker-compose
 apt install -y docker.io
 apt install -y docker-compose
 systemctl start docker
